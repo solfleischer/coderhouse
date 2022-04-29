4d81f7936834da160695d3c3e413bcd923945f93
solfleischer <88742150+solfleischer@users.noreply.github.com>
2022-04-29 17:30:14 -0300

    clase 2 - desafio

---
+++ coder/js/main.js
@@ -0,0 +1,57 @@
+
+    let bebidas = prompt("Ingrese cuál de estas bebidas quiere comprar: caramel machiatto, mocha helado, espresso americano o capuccino");
+    
+    
+    while (bebidas != ""){
+            
+                
+                if (bebidas=="caramel machiatto")
+                    {
+
+                        alert("El "+bebidas+" sale $500")
+                    }
+    
+                else if (bebidas=="mocha helado")
+                    {
+
+                        alert("El "+bebidas+" sale $550")
+                    }
+
+                else if (bebidas=="espresso americano")
+                    {
+
+                        alert("El "+bebidas+" sale $570")
+                    }
+                    
+                 else if (bebidas=="capuccino")
+                    {
+
+                        alert("El "+bebidas+" sale $600")
+                    }
+                    
+                else  
+                    {
+
+                        alert("Ingrese alguna bebida")
+
+                    }
+
+bebidas= prompt ("Ingrese otra de estas bebidas: caramel machiatto, mocha helado, espresso americano o capuccino ")
+
+                }
+
+
+            
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+        
