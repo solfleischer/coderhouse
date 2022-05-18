cfd654e2145a80562cef11148877a889c1ec284c
solfleischer <88742150+solfleischer@users.noreply.github.com>
2022-05-18 17:00:33 -0300

    coder

--- coder/js/main.js
+++ coder/js/main.js
@@ -1,123 +1,82 @@
+class Opcion {
+  constructor(servicio, precio, id) {
+    this.servicio = servicio;
+    this.precio = precio;
+    this.id = id;
+  }
+}
+
+
 let servicioId;
-const saludar = () => {
-    
-let nombre = prompt ("bienvenido, ingrese su nombre");
-
-while (nombre === ""){
-nombre = prompt ("bienvenido, ingrese su nombre");
-}
-
-return nombre;
-}
-saludar ();
-
-
-class opcion {
-  constructor (servicio, id){
-  this.servicio= servicio;
-  this.id=id;
-}
-}
-
-  const peluqueria = new opcion ("Peluquería", 1)
-  const manicura = new opcion ("Manicura", 2)
-  const pedicura = new opcion ("Pedicura", 3)
-  const depilacion = new opcion ("Depilación", 4)
-  const maquillaje = new opcion ("Maquillaje", 5)
-  const spa = new opcion ("Spa", 6)
-  const arrayservicios = [peluqueria, manicura, pedicura, depilacion, maquillaje, spa];
-
 
 let carrito = [];
 
-const mostrarServicios = () => { 
-let texto = ""; // iniciar variable texto
-arrayservicios.forEach((elemento)=>{
-texto += `${elemento.servicio} \n`;
-});
-alert (texto);
+const peluqueria = new Opcion ("Peluquería", 3000, 1);
+const manicura = new Opcion ("Manicura", 2000, 2);
+const pedicura = new Opcion ("Pedicura", 2000, 3);
+const depilacion = new Opcion ("Depilación", 1500, 4);
+const maquillaje = new Opcion ("Maquillaje", 2000, 5);
+const spa = new Opcion ("Spa", 1500, 6);
+const arrayservicios = [peluqueria, manicura, pedicura, depilacion, maquillaje, spa];
+
+
+
+const saludar = () => {
+  let nombre = prompt("Bienvenido, ingrese su nombre:");
+  while (nombre === "") {
+    nombre = prompt("Bienvenido, ingrese su nombre:");
+  }
+  console.log (nombre);
 };
 
-mostrarServicios ();
+const mostrarServicios = () => {
+  let texto = "";
+  arrayservicios.forEach((element) => {
+    texto += `${element.id}) ${element.servicio}\n`;
+  });
 
-function (elegir) {
-let seleccion = parseInt(prompt(`Elija un servicio :\n${texto}`));
-return seleccion; 
-}
+  let seleccion = parseInt(prompt(`Elija un servicio:\n${texto}`));
 
   while (seleccion < 1 || seleccion > 6) {
-    seleccion = parseInt (prompt(`Incorrecto !\n Elija un servicio :\n${texto}`));
-}
+    seleccion = parseInt(
+      prompt(`Incorrecto\nElija un servicio:\n${texto}`)
+    );
+  }
+  return seleccion;
+};
 
-
-function servicioSeleccionado(id) {
-  console.log(id);
+const servicioSeleccionado = (id) => {
   let serviFind = arrayservicios.find((element) => element.id === id);
   carrito.push(serviFind);
-}
+  console.log(serviFind);
+
+  let seguir = confirm("Desea elegir otro servicio? ");
+
+  if (seguir === true) {
+    init();
+  }
+};
 
 const init = () => {
   servicioId = mostrarServicios();
   servicioSeleccionado(servicioId);
 };
 
-init ();
-
-  let turno = prompt ("Ingrese fecha y horario del turno");
-  alert ("Eligió un turno para el" + " " + turno);
-  console.log (turno)
+saludar();
+init();
 
 
-  let precio = 3000
-  let pregunta = prompt ("¿Desea saber el costo?")
-  if (pregunta === "si" || pregunta === "Si" || pregunta === "SI") {
-          
-      
-        alert ("Su turno tiene un costo de " + precio)
-      }
-          else {
-              
-          }
 
-          function cliente (nombre, apellido, edad, dni){
+let turno = prompt ("Ingrese fecha y horario del turno");
+alert ("Eligió un turno para el" + " " + turno);
+console.log (turno)
 
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.edad = edad;
-            this.dni = dni;
-          
-          }
-          
-          const cliente1 = new cliente (
-            prompt("Ingresá tu nombre"),
-            prompt("Ingresá tu apellido"),
-            prompt("Ingresá tu edad"),
-            prompt("Ingresá tu número de documento"),
-          );
+
+let precio = ""
+let pregunta = prompt ("¿Desea saber el costo total?")
+
+if (pregunta === "si" || pregunta === "Si" || pregunta === "SI") {
         
-          console.log (cliente1);
-
-
-          if (solicitud === 1) {
-              alert(cliente1.nombre + ":" +" fuiste agendado para"+ opcion1.servicio +" el día " + turno + ", ¡Muchas gracias!")
-          } 
-          else if (solicitud === 2) {
-            alert(cliente1.nombre + ":" + " fuiste agendado para "+ opcion2.servicio +" el día " + turno + ", ¡Muchas gracias!")
-          } 
-          else if (solicitud === 3) {
-            alert(cliente1.nombre + ":" + " fuiste agendado para "+ opcion3.servicio +" el día " + turno + ", ¡Muchas gracias!")
-          } 
-          else if (solicitud === 4) {
-            alert(cliente1.nombre + ":" + " fuiste agendado para "+ opcion4.servicio +" el día " + turno + ", ¡Muchas gracias!")
-          } 
-          else if (solicitud === 5) {
-            alert(cliente1.nombre + ":" + " fuiste agendado para "+ opcion5.servicio +" el día " + turno + ", ¡Muchas gracias!")
-         } 
-         else if (solicitud <1 || solicitud >5 ){
-  alert("La opcion ingresada no es valida, intente mas tarde")
-}
-else {
-  alert ("Lo esperamos nuevamente")
-}
-
-
+    // sumar precio de los servicios seleccionados
+      
+};
