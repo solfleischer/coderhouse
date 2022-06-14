671f18a43cb025baccdda0386a4a5ca89c09f105
solfleischer <88742150+solfleischer@users.noreply.github.com>
2022-06-14 14:27:32 -0300

    coder

--- simulador/js/app.js
+++ simulador/js/app.js
@@ -166,5 +166,26 @@ AOS.init({
   });
 
 
+const $form= document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit (event){
+event.preventDefault()
+const form = new FormData(this)
+
+const response= await fetch(this.action,{
+method: this.method,
+body: form,
+headers:{
+  'Accept': 'application/json'
+}
+})
+
+if (response.ok){
+  this.reset()
+
+}
+}
  
  
